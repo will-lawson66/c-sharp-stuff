08e075fdae9f2c7d955ae16032aff3ac46d826b4
Bill Lawson <blawson@firstinspires.org>
2020-11-22 16:51:49 -0500

    asynchronous breakfast, except for the toast

--- c-sharp-stuff/c-sharp-stuff/Program.cs
+++ c-sharp-stuff/c-sharp-stuff/Program.cs
@@ -50,19 +50,24 @@ namespace c_sharp_stuff
             Coffee cup = BreakfastAsync.PourCoffee();
             Console.WriteLine("coffee is ready");
 
-            Egg eggs = await BreakfastAsync.FryEggsAsync(2);
-            Console.WriteLine("eggs are ready");
+            Task<Egg> eggsTask = BreakfastAsync.FryEggsAsync(2);
+            Task<Bacon> baconTask = BreakfastAsync.FryBaconAsync(3);
+            Task<Toast> toastTask = BreakfastAsync.ToastBreadAsync(2);
 
-            Bacon bacon = await BreakfastAsync.FryBaconAsync(3);
-            Console.WriteLine("bacon is ready");
-
-            Toast toast = await BreakfastAsync.ToastBreadAsync(2);
+            Toast toast = await toastTask;
             BreakfastAsync.ApplyButter(toast);
             BreakfastAsync.ApplyJam(toast);
             Console.WriteLine("toast is ready");
 
             Juice oj = BreakfastAsync.PourOJ();
             Console.WriteLine("oj is ready");
+
+            Egg eggs = await eggsTask;
+            Console.WriteLine("eggs are ready");
+
+            Bacon bacon = await baconTask;
+            Console.WriteLine("bacon is ready");
+
             Console.WriteLine("Breakfast is ready!");
         }
         #endregion
