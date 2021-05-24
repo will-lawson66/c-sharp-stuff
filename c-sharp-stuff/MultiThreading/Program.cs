8ce10c954060e87d23aa29f8608099a135e6ce25
Bill Lawson <bill.lawson@lumiradx.com>
2021-05-24 15:13:12 -0400

    reorganize; add multi-threading project

---
+++ c-sharp-stuff/MultiThreading/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a collection of tasks for processing
+            // create some threads to process the tasks
+        }
+    }
+}
